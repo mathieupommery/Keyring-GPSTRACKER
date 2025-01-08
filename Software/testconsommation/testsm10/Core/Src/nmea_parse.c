cabf757dc678f05cfd89473e94ae1bbff05b5932
mathieupommery <161692882+mathieupommery@users.noreply.github.com>
2025-01-08 22:50:00 +0100

    fe

---
+++ Software/testconsommation/testsm10/Core/Src/nmea_parse.c
@@ -0,0 +1,247 @@
+//
+// Created by sztuka on 22.01.2023.
+//
+#include "nmea_parse.h"
+#include "math.h"
+
+
+//code globalement recuperer, nous nous en somme inspiré afin de recuperer la vitesse sur la trame gnrmc, ce code se base sur des strtok afin de segmenter les trames puis sur des strtol
+//ou strtof afin d'en recuperer les informations et de les stocker dans la structure cf nmea_parsE.h
+//on a une fonction de decodage par typme de trame interressante, puis une fonction nmea_parse servant à mettre à jour la structure de donnée avec lesdonnées presentes dans le databuffer, qui lui se met à jour tout seul.
+
+char *data[15];
+
+int gps_checksum(char *nmea_data)
+{
+    //if you point a string with less than 5 characters the function will read outside of scope and crash the mcu.
+    if(strlen(nmea_data) < 5) return 0;
+    char recv_crc[2];
+    recv_crc[0] = nmea_data[strlen(nmea_data) - 4];
+    recv_crc[1] = nmea_data[strlen(nmea_data) - 3];
+    int crc = 0;
+    int i;
+
+    //exclude the CRLF plus CRC with an * from the end
+    for (i = 0; i < strlen(nmea_data) - 5; i ++) {
+        crc ^= nmea_data[i];
+    }
+    int receivedHash = strtol(recv_crc, NULL, 16);
+    if (crc == receivedHash) {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int nmea_GPGGA(GPS *gps_data, char*inputString){
+    char *values[25];
+    int counter = 0;
+    memset(values, 0, sizeof(values));
+    char *marker = strtok(inputString, ",");
+    while (marker != NULL) {
+        values[counter++] = malloc(strlen(marker) + 1); //free later!!!!!!
+        strcpy(values[counter - 1], marker);
+        marker = strtok(NULL, ",");
+    }
+    char lonSide = values[5][0];
+    char latSide = values[3][0];
+    strcpy(gps_data->lastMeasure, values[1]);
+    if(latSide == 'S' || latSide == 'N'){
+        char lat_d[2];
+        char lat_m[7];
+        for (int z = 0; z < 2; z++) lat_d[z] = values[2][z];
+        for (int z = 0; z < 6; z++) lat_m[z] = values[2][z + 2];
+
+        int lat_deg_strtol = strtol(lat_d, NULL, 10);
+        float lat_min_strtof = strtof(lat_m, NULL);
+        double lat_deg = lat_deg_strtol + lat_min_strtof / 60;
+
+        char lon_d[3];
+        char lon_m[7];
+
+        for (int z = 0; z < 3; z++) lon_d[z] = values[4][z];
+        for (int z = 0; z < 6; z++) lon_m[z] = values[4][z + 3];
+
+        int lon_deg_strtol = strtol(lon_d, NULL, 10);
+        float lon_min_strtof = strtof(lon_m, NULL);
+        double lon_deg = lon_deg_strtol + lon_min_strtof / 60;
+
+        if(lat_deg!=0 && lon_deg!=0 && lat_deg<90 && lon_deg<180){
+            gps_data->latitude = lat_deg;
+            gps_data->latSide = latSide;
+            gps_data->longitude = lon_deg;
+            gps_data->lonSide = lonSide;
+            float altitude = strtof(values[9], NULL);
+            gps_data->altitude = altitude!=0 ? altitude : gps_data->altitude;
+            gps_data->satelliteCount = strtol(values[7], NULL, 10);
+
+            int fixQuality = strtol(values[6], NULL, 10);
+            gps_data->fix = fixQuality > 0 ? 1 : 0;
+
+            float hdop = strtof(values[8], NULL);
+            gps_data->hdop = hdop!=0 ? hdop : gps_data->hdop;
+        }
+        else {
+            for(int i=0; i<counter; i++) free(values[i]);
+            return 0;
+        }
+
+    }
+
+    for(int i=0; i<counter; i++) free(values[i]);
+    return 1;
+}
+
+
+int nmea_GPGSA(GPS *gps_data, char*inputString){
+    char *values[25];
+    int counter = 0;
+    memset(values, 0, sizeof(values));
+    char *marker = strtok(inputString, ",");
+    while (marker != NULL) {
+        values[counter++] = malloc(strlen(marker) + 1); //free later!!!!!!
+        strcpy(values[counter - 1], marker);
+        marker = strtok(NULL, ",");
+    }
+    int fix = strtol(values[2], NULL, 10);
+    gps_data->fix = fix > 1 ? 1 : 0;
+    int satelliteCount = 0;
+    for(int i=3; i<15; i++){
+        if(values[i][0] != '\0'){
+            satelliteCount++;
+        }
+    }
+    gps_data->satelliteCount = satelliteCount;
+    for(int i=0; i<counter; i++) free(values[i]);
+    return 1;
+}
+
+
+
+int nmea_GNRMC(GPS *gps_data, char*inputString){
+    char *values[25];
+    int counter = 0;
+    memset(values, 0, sizeof(values));
+    char *marker = strtok(inputString, ",");
+    while (marker != NULL) {
+        values[counter++] = malloc(strlen(marker) + 1); //free later!!!!!!
+        strcpy(values[counter - 1], marker);
+        marker = strtok(NULL, ",");
+    }
+    float speed = strtof(values[7], NULL);
+    gps_data->speed=speed/(1.944);
+
+
+    for(int i=0; i<counter; i++) free(values[i]);
+    return 1;
+}
+
+
+
+void nmea_parse(GPS *gps_data, uint8_t *buffer){
+    memset(data, 0, sizeof(data));
+    char * token = strtok(buffer, "$");
+    int cnt = 0;
+    while(token !=NULL){
+        data[cnt++] = malloc(strlen(token)+1); //free later!!!!!
+        strcpy(data[cnt-1], token);
+        token = strtok(NULL, "$");
+    }
+    for(int i = 0; i<cnt; i++){
+       if(strstr(data[i], "\r\n")!=NULL && gps_checksum(data[i])){
+           if(strstr(data[i], "GNRMC")!=NULL){
+               nmea_GNRMC(gps_data, data[i]);
+           }
+           else if(strstr(data[i], "GNGSA")!=NULL){
+               nmea_GPGSA(gps_data, data[i]);
+           }
+           else if(strstr(data[i], "GNGGA")!=NULL){
+               nmea_GPGGA(gps_data, data[i]);
+           }
+       }
+
+    }
+    for(int i = 0; i<cnt; i++) free(data[i]);
+
+
+}
+
+void nmea_speed(GPS *gps_data, uint8_t *buffer){
+	 memset(data, 0, sizeof(data));
+	    char * token = strtok(buffer, "$");
+	    int cnt = 0;
+	    while(token !=NULL){
+	        data[cnt++] = malloc(strlen(token)+1); //free later!!!!!
+	        strcpy(data[cnt-1], token);
+	        token = strtok(NULL, "$");
+	    }
+	    for(int i = 0; i<cnt; i++){
+	       if(strstr(data[i], "\r\n")!=NULL && gps_checksum(data[i])){
+	           if(strstr(data[i], "GNRMC")!=NULL){
+	               nmea_GNRMC(gps_data, data[i]);
+	           }
+	           else if(strstr(data[i], "GNGSA")!=NULL){
+	                          nmea_GPGSA(gps_data, data[i]);
+	                      }
+	       }
+
+	    }
+	    for(int i = 0; i<cnt; i++) free(data[i]);
+
+}
+
+
+
+double distancecalc(double lat1, double lat2, double long1, double long2){
+	double distance=0;
+	distance=(double) 6371000*acosl(fmin(1,sinl(lat1*(M_PI/180))*sinl(lat2*(M_PI/180))+cosl(lat1*(M_PI/180))*cosl(lat2*(M_PI/180))*cosl((long2-long1)*(M_PI/180))));
+
+	return distance;
+}
+
+uint16_t checksumcalculate(uint8_t * cmdbuffer){
+	int index=0;
+	uint16_t checksum=0;
+	uint8_t checksum1=0;
+	uint8_t checksum2=0;
+	while(cmdbuffer[index]!='$'){
+		index++;
+	}
+	index++;
+	while(cmdbuffer[index]!='*'){
+		checksum^=cmdbuffer[index];
+		index++;
+	}
+	index++;
+	checksum1=(uint8_t) floor(checksum/16);
+	checksum2=(uint8_t) (checksum%16);
+
+	if(checksum1==0){
+		cmdbuffer[index]=checksum2;
+	}
+	else{
+		cmdbuffer[index]=checksum1;
+		index++;
+		cmdbuffer[index]=checksum2;
+	}
+	index++;
+	cmdbuffer[index]='\r';
+	index++;
+	cmdbuffer[index]='\n';
+	return checksum;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Software/testconsommation/testsm10/Core/Src/usart.c
@@ -0,0 +1,266 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    usart.c
+  * @brief   This file provides code for the configuration
+  *          of the USART instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "usart.h"
+
+/* USER CODE BEGIN 0 */
+#include "string.h"
+
+extern uint8_t RxBuffer[RxBuffer_SIZE];
+extern uint8_t DataBuffer[DataBuffer_SIZE];
+extern uint16_t oldPos;
+extern uint16_t newPos;
+/* USER CODE END 0 */
+
+UART_HandleTypeDef huart4;
+UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_uart4_rx;
+
+/* UART4 init function */
+void MX_UART4_Init(void)
+{
+
+  /* USER CODE BEGIN UART4_Init 0 */
+
+  /* USER CODE END UART4_Init 0 */
+
+  /* USER CODE BEGIN UART4_Init 1 */
+
+  /* USER CODE END UART4_Init 1 */
+  huart4.Instance = UART4;
+  huart4.Init.BaudRate = 9600;
+  huart4.Init.WordLength = UART_WORDLENGTH_8B;
+  huart4.Init.StopBits = UART_STOPBITS_1;
+  huart4.Init.Parity = UART_PARITY_NONE;
+  huart4.Init.Mode = UART_MODE_TX_RX;
+  huart4.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart4.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart4.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart4.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN UART4_Init 2 */
+
+  /* USER CODE END UART4_Init 2 */
+
+}
+/* USART2 init function */
+
+void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  if(uartHandle->Instance==UART4)
+  {
+  /* USER CODE BEGIN UART4_MspInit 0 */
+
+  /* USER CODE END UART4_MspInit 0 */
+
+  /** Initializes the peripherals clock
+  */
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_UART4;
+    PeriphClkInit.Uart4ClockSelection = RCC_UART4CLKSOURCE_PCLK1;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* UART4 clock enable */
+    __HAL_RCC_UART4_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    /**UART4 GPIO Configuration
+    PA1     ------> UART4_RX
+    PC10     ------> UART4_TX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF8_UART4;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF8_UART4;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /* UART4 DMA Init */
+    /* UART4_RX Init */
+    hdma_uart4_rx.Instance = DMA2_Channel5;
+    hdma_uart4_rx.Init.Request = DMA_REQUEST_2;
+    hdma_uart4_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_uart4_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_uart4_rx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_uart4_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_uart4_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_uart4_rx.Init.Mode = DMA_CIRCULAR;
+    hdma_uart4_rx.Init.Priority = DMA_PRIORITY_LOW;
+    if (HAL_DMA_Init(&hdma_uart4_rx) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(uartHandle,hdmarx,hdma_uart4_rx);
+
+  /* USER CODE BEGIN UART4_MspInit 1 */
+
+  /* USER CODE END UART4_MspInit 1 */
+  }
+  else if(uartHandle->Instance==USART2)
+  {
+  /* USER CODE BEGIN USART2_MspInit 0 */
+
+  /* USER CODE END USART2_MspInit 0 */
+
+  /** Initializes the peripherals clock
+  */
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+    PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* USART2 clock enable */
+    __HAL_RCC_USART2_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN USART2_MspInit 1 */
+
+  /* USER CODE END USART2_MspInit 1 */
+  }
+}
+
+void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
+{
+
+  if(uartHandle->Instance==UART4)
+  {
+  /* USER CODE BEGIN UART4_MspDeInit 0 */
+
+  /* USER CODE END UART4_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_UART4_CLK_DISABLE();
+
+    /**UART4 GPIO Configuration
+    PA1     ------> UART4_RX
+    PC10     ------> UART4_TX
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_1);
+
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_10);
+
+    /* UART4 DMA DeInit */
+    HAL_DMA_DeInit(uartHandle->hdmarx);
+  /* USER CODE BEGIN UART4_MspDeInit 1 */
+
+  /* USER CODE END UART4_MspDeInit 1 */
+  }
+  else if(uartHandle->Instance==USART2)
+  {
+  /* USER CODE BEGIN USART2_MspDeInit 0 */
+
+  /* USER CODE END USART2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART2_CLK_DISABLE();
+
+    /**USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX
+    */
+    HAL_GPIO_DeInit(GPIOA, USART_TX_Pin|USART_RX_Pin);
+
+  /* USER CODE BEGIN USART2_MspDeInit 1 */
+
+  /* USER CODE END USART2_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+	oldPos = newPos; //keep track of the last position in the buffer
+			if(oldPos + 64 > DataBuffer_SIZE){ //if the buffer is full, parse it, then reset the buffer
+
+				uint16_t datatocopy = DataBuffer_SIZE-oldPos;  // find out how much space is left in the main buffer
+				memcpy ((uint8_t *)DataBuffer+oldPos, RxBuffer, datatocopy);  // copy data in that remaining space
+
+				oldPos = 0;  // point to the start of the buffer
+				memcpy ((uint8_t *)DataBuffer, (uint8_t *)RxBuffer+datatocopy, (64-datatocopy));  // copy the remaining data
+				newPos = (64-datatocopy);  // update the position
+			}
+			else{
+				memcpy((uint8_t *)DataBuffer+oldPos, RxBuffer, 64); //copy received data to the buffer
+				newPos = 64+oldPos; //update buffer position
+
+			}
+			HAL_UART_Receive_DMA(&huart4, (uint8_t *)RxBuffer, RxBuffer_SIZE);//on recoit par dma à nouveau 64 caractères
+			__HAL_DMA_DISABLE_IT(&hdma_uart4_rx, DMA_IT_HT);//on desactive l'interruption afin de ne pas être interrompu tout le temps
+
+	//HAL_UART_Receive_DMA(&huart4, (uint8_t *)RxBuffer, RxBuffer_SIZE);//l'appel de cette fonction réactive l'intérruption.
+}
+/* USER CODE END 1 */
